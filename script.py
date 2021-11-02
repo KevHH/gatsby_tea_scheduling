c549a2e3a638107adc9ff9f55269c159483cf627
Kevin Huang <huang.han.kev@gmail.com>
2021-11-02 21:38:01 +0800

    naive logic for testing

--- script.py
+++ script.py
@@ -8,6 +8,7 @@ def scheduling(empty_schedule, people):
     empty_schedule : list of dictionaries with the following keys
         'date': datetime.datetime object representing date of the talk
         'type': either 'R' or 'T' representing research talk or tea talk
+        'id': string, representing notion id of the talk
     people : list of dictionaries with the following keys
         'id': string, representing notion id of the person
         'away_dates': list of 2-tuples of datetime.datetime objects representing start and end of away dates (inclusive)  
@@ -17,7 +18,17 @@ def scheduling(empty_schedule, people):
     filled_schedule: list of dictionaries with the following keys
         'date': datetime.datetime object representing date of the talk
         'type': either 'R' or 'T' representing research talk or tea talk
+        'id': string, representing notion id of the talk
         'presenter': notion id for the person presenting
         'tea': notion id for the person doing tea preparation
     """
-    print("")
+    
+    filled_schedule = []
+    for talk in empty_schedule:
+        filled_schedule.append({'date':talk['date'],
+                                'type':talk['type'],
+                                'id':talk['id'],
+                                'presenter':people[0]['id'],
+                                'tea':people[0]['id']  
+                                })
+    return filled_schedule
